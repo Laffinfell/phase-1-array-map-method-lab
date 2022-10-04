43bdfb4ba56bfd10714f71674b1e67fda60a706a
Andrew <afellman84@gmail.com>
2022-10-04 18:11:57 -0400

    commit tree 7e2a141987d602d7db70cc6dcb7a77ad77741e78

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -10,7 +16,45 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(str => {
+  const words = str.split(" ");
+  const capitalizeWords = words.map(word => word[0].toUpperCase() + word.slice(1));
+  return capitalizeWords.join(" ")
+  })}
+
+
+
+
+
+// const listOfStrings = strings.map((string) => {
+//   return Object.assign({}, string, {
+//     function capitalizeFirstLetter(str) {
+//       return str.charAt(0).toUpperCase() + str.slice(1);
+//     }
+//   });
+// }
+
+
+
+
+
+// let sentences = messages.split(/[,,,,]/);
+// console.log(sentences);
+
+// function capitalizeFirstLetter(str) {
+//   return str.charAt(0).toUpperCase() +
+//  str.slice(1);
+// }
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// capitalizeFirstLetter(string);
+
+
+// const elements = tutorials.map((p) => ({
+//   ...p,
+//   element: `${String}` 
+// }));
